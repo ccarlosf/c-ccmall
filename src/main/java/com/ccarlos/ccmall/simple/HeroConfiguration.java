ae892c67559475977b9b3c76c397b2619345eb43
ccarlos <2238513100@qq.com>
2020-02-08 10:22:22 +0800

    (10 3-3) @Configuration是用来替换bean的xml配置 (10 3-4) 最终变化要被隔离到配置文件中

--- src/main/java/com/ccarlos/ccmall/simple/HeroConfiguration.java
+++ src/main/java/com/ccarlos/ccmall/simple/HeroConfiguration.java
@@ -8,11 +8,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class HeroConfiguration {
 
+    /**
+     * @description: 实例化camile
+     * @author: ccarlos
+     * @date: 2020/2/8 10:17
+     * @return: com.ccarlos.ccmall.simple.ISkill
+     */
     @Bean
     public ISkill camile(){
         return new Camile("Camile",18);
     }
 
+    /**
+     * @description: 实例化irelia
+     * @author: ccarlos
+     * @date: 2020/2/8 10:17
+     * @return: com.ccarlos.ccmall.simple.ISkill
+     */
     @Bean
     public ISkill irelia(){
         return new Irelia();
