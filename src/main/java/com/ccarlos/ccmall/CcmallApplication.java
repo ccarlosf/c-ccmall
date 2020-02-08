9e44d0cbd475a8980337334ef1a760ff6aea3799
ccarlos <2238513100@qq.com>
2020-02-08 13:20:38 +0800

    (11 1-2) @ComponentScan包扫描机制

--- src/main/java/com/ccarlos/ccmall/CcmallApplication.java
+++ src/main/java/com/ccarlos/ccmall/CcmallApplication.java
@@ -2,8 +2,10 @@ package com.ccarlos.ccmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.ccarlos")
 public class CcmallApplication {
 
     public static void main(String[] args) {
