7d23787b14ffb3efef66a46d2564cfd71c9e9743
ccarlos <2238513100@qq.com>
2020-02-08 17:53:26 +0800

    (11 2-5) @Import的两种用法

--- src/main/java/com/ccarlos/ccmall/LOLApplication.java
+++ src/main/java/com/ccarlos/ccmall/LOLApplication.java
@@ -1,14 +1,22 @@
 package com.ccarlos.ccmall;
 
+import com.ccarlos.ccmall.simple.HeroConfiguration;
 import com.ccarlos.ccmall.simple.ISkill;
+import com.ccarlos.ccmall.simple.LOLConfigurationSelector;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Import;
 
+//@ComponentScan
+//@Import(HeroConfiguration.class)
+@Import(LOLConfigurationSelector.class)
 public class LOLApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context =
                 new SpringApplicationBuilder(LOLApplication.class)
+                        .web(WebApplicationType.NONE)
                 .run(args);
         ISkill iSkill = (ISkill) context.getBean("irelia");
         iSkill.r();
