2a9fe97ce708e32a2b52b81eb83a5c7e70f57548
ccarlos <2238513100@qq.com>
2020-02-08 21:50:35 +0800

    (12 2-1) 使用注解修改HTTPStatus

--- src/main/java/com/ccarlos/ccmall/core/GlobalExceptionAdvice.java
+++ src/main/java/com/ccarlos/ccmall/core/GlobalExceptionAdvice.java
@@ -1,9 +1,11 @@
 package com.ccarlos.ccmall.core;
 
 import com.ccarlos.ccmall.exception.http.HttpException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -23,8 +25,12 @@ public class GlobalExceptionAdvice {
      */
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
+    @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
     public UnifyResponse handleException(HttpServletRequest req, Exception e) {
-        UnifyResponse message = new UnifyResponse(9999,"服务器异常","url");
+        String requestUrl = req.getRequestURI();
+        String method = req.getMethod();
+        System.out.println(e);
+        UnifyResponse message = new UnifyResponse(9999,"服务器异常",method+" "+requestUrl);
         return message;
     }
     
