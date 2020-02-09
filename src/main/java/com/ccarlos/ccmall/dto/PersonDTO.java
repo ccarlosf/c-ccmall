a5f749335f92438794f9ad82e083bdc6760f3b3f
ccarlos <2238513100@qq.com>
2020-02-09 15:52:39 +0800

    (13 1-5) LomBok基本使用方式

--- src/main/java/com/ccarlos/ccmall/dto/PersonDTO.java
+++ src/main/java/com/ccarlos/ccmall/dto/PersonDTO.java
@@ -1,27 +1,37 @@
 package com.ccarlos.ccmall.dto;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @description: 用户数据传输对象
  * @author: ccarlos
  * @date: 2020/2/9 14:21
  */
+@Getter
+@Setter
+//@Data
+//@EqualsAndHashCode
+// equals hashCode toString
 public class PersonDTO {
     private String name;
     private Integer age;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public Integer getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(Integer age) {
+//        this.age = age;
+//    }
 }
